9874255c189c493d914a32a869f18364639cfb2d
Welcome ! <usmanubaid9@gmail.com>
2023-09-17 00:05:20 +0200

    feat: use useState hook

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -1,7 +1,15 @@
+import Pet from "./Pet";
+import "./App.css";
+import SearchParams from "./SearchParams";
+
 function App() {
   return (
     <div>
-      <h1>Rendered</h1>
+      <h1>Adopt Me!</h1>
+      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cocktiel" />
+      <Pet name="Doink" animal="cat" breed="Mixed" /> */}
+      <SearchParams />
     </div>
   );
 }
