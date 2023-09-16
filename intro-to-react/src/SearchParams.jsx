a9e5e5365fae7a4b491d122caeff468be97e6d2c
Welcome ! <usmanubaid9@gmail.com>
2023-09-17 00:25:30 +0200

    feat: rendering list

--- intro-to-react/src/SearchParams.jsx
+++ intro-to-react/src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+
+  const breeds = ["poodle"];
   return (
     <div className="search-params">
       <form>
@@ -14,6 +19,31 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => {
+            setAnimal(e.target.value);
+            setBreed("");
+          }}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option key={animal}>{animal}</option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {breeds.map((breed) => (
+            <option key={breed}>{breed}</option>
+          ))}
+        </select>
         <button>Submit</button>
       </form>
     </div>
