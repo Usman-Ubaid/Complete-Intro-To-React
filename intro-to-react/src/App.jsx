3f20548f5d524a3d95cf59441fcea7aa612cb403
Welcome ! <usmanubaid9@gmail.com>
2023-09-23 00:43:42 +0200

    feat: use useContext hook

--- intro-to-react/src/App.jsx
+++ intro-to-react/src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
+import AdoptedPetContext from "./AdoptedPetContext";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
 function App() {
+  const adoptedPet = useState();
   return (
     <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </AdoptedPetContext.Provider>
     </div>
   );
 }
--- intro-to-react/src/SearchParams.jsx
+++ intro-to-react/src/SearchParams.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import fetchSearch from "./fetchSearch";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
@@ -13,6 +14,7 @@ const SearchParams = () => {
     breed: "",
   });
   const [animal, setAnimal] = useState("");
+  const [adoptedPet] = useContext(AdoptedPetContext);
   const results = useQuery(["search", requestParams], fetchSearch);
   const pets = results?.data?.pets ?? [];
 
@@ -32,6 +34,11 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
           <input id="location" name="location" placeholder="Location" />
